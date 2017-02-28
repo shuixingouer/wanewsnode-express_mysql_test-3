5d281f283cb5cf9a4a0be6857a714669aa2c3443
shuixing <shuixing@ixiaopu.com>
2017-02-28 19:11:47 +0800

    添加频道对应的列表接口

--- sql/sqlMap.js
+++ sql/sqlMap.js
@@ -3,7 +3,7 @@ var sqlMap = {
     // ”√ªß
     channel:{
         list:'select * from channel',
-        num:'select a.id,a.title,a.type,a.author,a.created_at,a.src_url,b.channel_id from object_detail a,channel_object_rel b where a.id=b.object_id  and channel_id=?'
+        num:'select a.title,a.publish_time,b.channel_id ,d.file_id,c.image_src_url from object_detail a, channel_object_rel b, object_images_rel c, image d where a.id = b.object_id and c.object_id=b.object_id and d.id=c.image_id and channel_id=? limit 0,10'
     }
 }
 module.exports = sqlMap;
